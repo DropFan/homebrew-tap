ba85496589ec90928aebf0b7536a519ebd0d242b
Tiger <DropFan@Gmail.com>
2026-01-26 22:00:55 +0800

    codelens 0.0.2

--- Formula/codelens.rb
+++ Formula/codelens.rb
@@ -1,28 +1,28 @@
 class Codelens < Formula
   desc "High performance code statistics tool written in Rust"
   homepage "https://github.com/DropFan/codelens"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/DropFan/codelens/releases/download/v0.0.1-rust/codelens-0.0.1-x86_64-apple-darwin.tar.gz"
-      sha256 "6cff95327891a877df1cd4d9d6fc62fb81bb53627a5fb621a1814361b0a85092"
+      url "https://github.com/DropFan/codelens/releases/download/v0.0.2-rust/codelens-0.0.2-x86_64-apple-darwin.tar.gz"
+      sha256 "3853caba38b2284133b8051ade253867caa1d150db5eca599d04bc7d9bf05230"
     end
     on_arm do
-      url "https://github.com/DropFan/codelens/releases/download/v0.0.1-rust/codelens-0.0.1-aarch64-apple-darwin.tar.gz"
-      sha256 "c4d888f4bfa2434e5b4362faa466caf706f6e0e890ab5611655161d5009b1516"
+      url "https://github.com/DropFan/codelens/releases/download/v0.0.2-rust/codelens-0.0.2-aarch64-apple-darwin.tar.gz"
+      sha256 "7b7b4c0d33bbe4b7ea3c4f0bbbab2ac0d55f8db8b095921298498fac9b1e10eb"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/DropFan/codelens/releases/download/v0.0.1-rust/codelens-0.0.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "78926e7b687ec7e01240de8d13914efc669547efab3c4c88afed089d2dbabbf3"
+      url "https://github.com/DropFan/codelens/releases/download/v0.0.2-rust/codelens-0.0.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "c3b1a469917b7260cc9e250ea677e2e0c482ed4fe98a7da6303d61d004f99146"
     end
     on_arm do
-      url "https://github.com/DropFan/codelens/releases/download/v0.0.1-rust/codelens-0.0.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9c086f714216f6af89671d4aea9725b4cc3d7c081764e1e58aaba731aeef9806"
+      url "https://github.com/DropFan/codelens/releases/download/v0.0.2-rust/codelens-0.0.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "85e29050e0f9e46ebde739b3a62020298bb256b50215171c8887286584510b28"
     end
   end
 
@@ -43,4 +43,3 @@ class Codelens < Formula
     assert_match "codelens", shell_output("#{bin}/codelens --version")
   end
 end
-
